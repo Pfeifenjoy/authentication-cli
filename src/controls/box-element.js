7384c4c7ca70992e8f1f20dbd1083298772d539a
Arwed Mett <arwed.mett@googlemail.com>
2016-12-23 17:37:44 +0100

    nicer layout

--- src/controls/box-element.js
+++ src/controls/box-element.js
@@ -3,6 +3,7 @@ import styled from "styled-components"
 const MARGIN = 0.4
 
 export default Component => styled(Component)`
-    width: calc(100% - ${ MARGIN * 2 }em);
-    margin: ${ MARGIN }em;
+    width: 100%;
+    margin-top: ${ MARGIN }em;
+    margin-bottom: ${ MARGIN }em;
 `
